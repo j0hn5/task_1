299a1d07179b764e295bb6e41aeaf2b21231182d
john.shand <john.shand@sykescottages.co.uk>
2018-05-22 15:48:12 +0100

    Added cucumber, junit, selenium  and other initial dependencies, written feature file and runner class

---
+++ src/main/java/TestRunner/CucumberRunner.java
@@ -0,0 +1,10 @@
+package TestRunner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "Features", glue = "Steps")
+public class CucumberRunner {
+}
